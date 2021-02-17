c0a8a087414c113f551f1025050546896c2995bf
Patrick <patrickvyn@gmail.com>
2021-02-17 14:25:53 -0500

    EnemySpawn

---
+++ Assets/Scripts/Enemy/Spawner/Spawner_TopValley.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner_TopValley : MonoBehaviour
+{
+
+    [SerializeField]
+    int enemyCount;
+
+    public GameObject[] enemyPF;
+    int xPos;
+    int yPos;
+    
+        void Start()
+    {
+        StartCoroutine(EnemySpawn());
+    }
+
+    IEnumerator EnemySpawn()
+    {
+        while(enemyCount < 5){
+            int randEnemy = Random.Range(0, enemyPF.Length);
+
+            xPos= Random.Range(-25, 39);
+            yPos= Random.Range( 9, 25);
+            Instantiate(enemyPF[randEnemy], new Vector2(xPos, yPos), transform.rotation);
+            yield return new WaitForSeconds(2f);
+            enemyCount += 1;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public float mvmt_speed;
+    public float move_x;
+    public float move_y;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private float someScale;
+    public Animator playerAnim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+        someScale = transform.localScale.x; // assuming this is facing right
+        movement = new Vector2 (0, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+
+    
+        move_x = Input.GetAxis("Horizontal");
+        playerAnim.SetFloat("Speed", Mathf.Abs(move_x));
+           
+        move_y = Input.GetAxis("Vertical");
+        movement = new Vector2 (move_x * mvmt_speed, move_y * mvmt_speed);
+    }
+
+
+    void FixedUpdate(){
+
+        rb.MovePosition(rb.position + movement);
+        FlipPlayer();
+        
+    }
+    
+    /*
+    Flip Player on Y-axis dependant on x move_x value
+    */
+    void FlipPlayer(){
+
+        
+        if(move_x < 0){
+            transform.localScale = new Vector2(-someScale, transform.localScale.y);
+        } else if(move_x > 0){
+            transform.localScale = new Vector2(someScale, transform.localScale.y);
+        }
+      
+    }
+
+
+
+
+   
+
+}
+ 
