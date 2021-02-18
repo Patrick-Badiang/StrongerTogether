3b1450ed6c75f4dd047622b0a7dbd57f4f632ba2
Patrick <patrickvyn@gmail.com>
2021-02-17 20:26:51 -0500

    EnemySpawner and Introducing EnemyPathFinding

---
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    
+    private Vector3 startingPosition;
+    private Vector3 roamPoistion;
+
+    // private void Start()
+    // {
+    //     startingPosition = transform.position;
+    //     roamPoistion = GetRoamingPosition();
+    // }
+
+    // private void Update(){
+        
+    // }
+
+    // private Vector3 GetRoamingPosition(){
+    //     // return startingPosition +UtilsClass.GetRandomDir() * Random.Range(10f,70f); 
+    // }
+
+}
+
